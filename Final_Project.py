c24051a59c8b2963ab766979b160c75ed959a770
Edwin S. Torres Cuevas <51466453+edwinstc@users.noreply.github.com>
2022-05-06 20:58:49 -0500

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -18,7 +18,7 @@ def get_il_family(cation):
     for j in sheetnames[2:]: 
          if j[8:].lower() in cation_abbrev.get(cation): 
             sheet_index = (sheetnames.index(j))
-         #elif cation=='MTBD' or cation=='TMG':
+        #  elif cation=='MTBD' or cation=='TMG':
             #sheet_index = 8
          #else: sheet_index = 9         
     
