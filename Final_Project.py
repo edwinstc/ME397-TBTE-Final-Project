0aa994b6ebb1b2dc2a4c84950a8d9d1448249499
Edwin S. Torres Cuevas <51466453+edwinstc@users.noreply.github.com>
2022-05-10 00:19:23 -0500

    Update Final_Project.py

--- Final_Project.py
+++ Final_Project.py
@@ -36,19 +36,22 @@ def swap_columns(df, col1, col2): ##Snippet from https://www.statology.org/swap-
 def display_regs(df,prop_col):
     for k in df['Full Reference'].unique():
         short_ref = k.split(", ")[0]
+        #Figure out which type of fit to use
         if prop_col=='Density (g/cm3)':
             Y = np.array(df.loc[df['Full Reference']==k][f'{prop_col}'])
             X = np.array(df.loc[df['Full Reference']==k]['T /K']).reshape(-1,1)
-            
         else:
             Y = np.array(np.log(df.loc[df['Full Reference']==k][f'{prop_col}']))
             X = np.array(1000/(df.loc[df['Full Reference']==k]['T /K'])).reshape(-1,1)
-            
+        #FIt the model
         model = LinearRegression().fit(X,Y)
+        #Choose type of equation to display
         if prop_col=='Density (g/cm3)':
             eqn = f'\u03c1_{short_ref}={model.coef_[0]:.4e}*T+{model.intercept_:.4f}'
         else:
             eqn = f'ln(\u03bc_{short_ref})={model.coef_[0]:.4e}*1000/T+{model.intercept_:.4f}'
+        #Display equation
+        stl.write(eqn)
 
 #Make webapp inputs
 stl.title("Brennecke Group Internal IL Database")
