6748943659ae20c1fb4e7ba00b6452e20810e439
Edwin S. Torres Cuevas <51466453+edwinstc@users.noreply.github.com>
2022-05-09 23:14:45 -0500

    Added regression calculation

--- Final_Project.py
+++ Final_Project.py
@@ -3,16 +3,18 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
-# from scipy import stats ##Use to implement trendline calculation
+from sklearn.linear_model import LinearRegression ##Use to implement trendline calculation
 
 spreadsheet = pd.ExcelFile('Density_05-01-2020.xlsx')
 sheetnames = spreadsheet.sheet_names
 
-#Make Database of ILs
+#Make Database of ILs and properties
 df_ILs = spreadsheet.parse(sheetnames[1],skiprows=4)
 cation_abbrev = dict(zip(df_ILs["Abbreviations"],df_ILs["Names"]))
 anion_abbrev = dict(zip(df_ILs["Abbreviations.1"],df_ILs["Names.1"]))
+dict_props = {"Density":"Density (g/cm3)","Viscosity":"Viscosity (cP)"}
 
+#Define functions used later
 def get_il_family(cation):
     sheet_index = None
 
@@ -31,24 +33,33 @@ def swap_columns(df, col1, col2): ##Snippet from https://www.statology.org/swap-
     df = df[col_list]
     return df
 
+def display_regs(df,prop_col):
+    for k in df['Full Reference'].unique():
+        Y = np.array(df.loc[df['Full Reference']==k][f'{prop_col}'])
+        X = np.array(df.loc[df['Full Reference']==k]['T /K']).reshape(-1,1)
+        short_ref = k.split(", ")[0]
+        model = LinearRegression().fit(X,Y)
+        stl.write(f'\u03c1_{short_ref}={model.coef_[0]:.4e}*T+{model.intercept_:.4f}')
+        
+
+#Make webapp inputs
 stl.title("Brennecke Group Internal IL Database")
 cation = stl.selectbox(label="Please select IL cation:",options=cation_abbrev.keys())
 anion = stl.selectbox(label="Please select IL anion:",options=anion_abbrev.keys())
 prop = stl.selectbox(label="Which property would you like to see?",options = ['Density','Viscosity'])
-dict_props = {"Density":"Density (g/cm3)","Viscosity":"Viscosity (cP)"}
-prop_column = dict_props.get(prop)
-
-datasheet = pd.ExcelFile(f'{prop}_05-01-2020.xlsx')
-sheetnames = datasheet.sheet_names
-
 # cation = 'bmim' ##Test case cation. Also try P2224, Pyrro14, emim
 # anion = 'Tf2N' ##Test case anion. Also try BF4, PF6, 2CNPyrro
+
+#Choose property and file to read
+prop_column = dict_props.get(prop)
+datasheet = pd.ExcelFile(f'{prop}_05-01-2020.xlsx')
+sheetnames = datasheet.sheet_names
 
 #Get Property Data
 il_input = f"[{cation}][{anion}]"
 il_family = get_il_family(cation)
 stl.markdown(f"This IL's full name is {cation_abbrev.get(cation)} {anion_abbrev.get(anion)}.")
-          
+       
 if il_input in list(il_family['IL']):
     il_dens = il_family.loc[il_family["IL"] == il_input][['T /K',f'{prop_column}','Full Reference']]
     il_dens.replace(np.nan,None,inplace=True)
@@ -64,8 +75,9 @@ if il_input in list(il_family['IL']):
     #Get correlation
     # slope, intercept, r_value, p_value, std_err = stats.linregress(il_dens['T /K'],il_dens['Density (g/cm3)'])
        
-    fig = plt.figure(figsize=(10,4))
-    sns.scatterplot(data=il_dens,x='T /K',y = f'{prop_column}', hue = 'Ref').set(title=f'{prop} of {il_input}')
+    # fig = plt.figure(figsize=(10,4))
+    fig = sns.lmplot(data=il_dens,x='T /K',y = f'{prop_column}', hue = 'Ref', ci=False).set(title=f'{prop} of {il_input}')
+    display_regs(il_dens,f'{prop_column}')
     stl.pyplot(fig)
     stl.write('## Data:')
     stl.write(il_dens.drop('Ref',axis=1))
@@ -73,3 +85,4 @@ if il_input in list(il_family['IL']):
     for j in unique_refs: stl.write(j)
 else:
     stl.write(f"{il_input} is currently **not** in our database. :confused: Try [ILthermo](https://ilthermo.boulder.nist.gov/)! Remember they use full IL name! :smile:")
+# %%
